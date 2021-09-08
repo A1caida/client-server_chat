f96da8a1e84b610e75a0f0414d23fb91b70720a0
A1caida <nimmt74@gmail.com>
2021-09-08 20:43:15 +0400

    ped

---
+++ client-server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+
+            try
+            {
+                socket.Bind(iPEndPoint);
+
+                socket.Listen(5);
+
+                Console.WriteLine("da");
+
+                while (true)
+                {
+                    Socket sock = socket.Accept();
+
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    do
+                    {
+                        bytes = sock.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (sock.Available > 0);
+
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+                    //string message = "ты гей";
+                    //data = Encoding.Unicode.GetBytes(message);
+                    sock.Send(data);
+                    sock.Shutdown(SocketShutdown.Both);
+                    sock.Close();
+                }
+                //sock.Shutdown(SocketShutdown.Both);
+                //sock.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                while (true)
+                {
+                    socket.Connect(iPEndPoint);
+                    Console.Write("Введите сообщение:");
+                    string msg = Console.ReadLine();
+                    byte[] data = Encoding.Unicode.GetBytes(msg);
+                    socket.Send(data);
+
+
+                    data = new byte[256];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (socket.Available > 0);
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+
+        }
+    }
+}
